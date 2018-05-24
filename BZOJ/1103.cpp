84b9bc8a8cafac6ec5a2aec45f9958c47198d7e7
wcysai <wcysai@foxmail.com>
2018-05-25 00:48:52 +0800

    finished EC-Final 17

--- BZOJ/1103.cpp
+++ BZOJ/1103.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#define MAXN 20005
+#define MAXN 400005
 #define INF 1000000000
 #define MOD 1000000007
 #define F first
@@ -7,15 +7,11 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-struct edge
-{
-    int to,cost;
-};
 struct node
 {
-    int l,r,maxi;
+    int l,r,maxi,sum;
 };
-int t,tot,n,k,a[MAXN],u[MAXN],v[MAXN],cost[MAXN];
+int tot,q,n,k;
 int pa[MAXN],dep[MAXN],sz[MAXN],wson[MAXN],top[MAXN],spos[MAXN],tpos[MAXN];
 struct segtree
 {
@@ -26,22 +22,24 @@ struct segtree
         seg[k].l=l;seg[k].r=r;
         if(l==r)
         {
-            seg[k].maxi=a[tpos[l]];
+            seg[k].maxi=seg[k].sum=l==1?0:1;
             id[l]=k;
             return;
         }
         int mid=(l+r)/2;
         build(k*2,l,mid);build(k*2+1,mid+1,r);
         seg[k].maxi=max(seg[k*2].maxi,seg[k*2+1].maxi);
+        seg[k].sum=seg[k*2].sum+seg[k*2+1].sum;
     }      
     void update(int k,int x)
     {
         k=id[k];
-        seg[k].maxi=x;
+        seg[k].maxi+=x;seg[k].sum+=x;
         while(k>1)
         {
             k=k/2;
             seg[k].maxi=max(seg[k*2].maxi,seg[k*2+1].maxi);
+            seg[k].sum=seg[k*2].sum+seg[k*2+1].sum;
         }
     }
     int query1(int k,int l,int r)
@@ -50,20 +48,29 @@ struct segtree
         if(seg[k].l>=l&&seg[k].r<=r) return seg[k].maxi;
         return max(query1(k*2,l,r),query1(k*2+1,l,r));
     }
+    int query2(int k,int l,int r)
+    {
+        if(seg[k].l>r||seg[k].r<l) return 0;
+        if(seg[k].l>=l&&seg[k].r<=r) return seg[k].sum;
+        return query2(k*2,l,r)+query2(k*2+1,l,r);
+    }
     int query_max(int l,int r) 
     {
         return query1(1,l,r);
     }
+    int query_sum(int l,int r)
+    {
+        return query2(1,l,r);
+    }
 }tree;
-vector<edge> G[MAXN];
+vector<int> G[MAXN];
 void dfs1(int v,int p,int d)
 {
     dep[v]=d;pa[v]=p;sz[v]=1;
     for(int i=0;i<G[v].size();i++)
     {
-        int to=G[v][i].to;
+        int to=G[v][i];
         if(to==p) continue;
-        a[to]=G[v][i].cost;
         dfs1(to,v,d+1);
         if(sz[to]>sz[wson[v]]) wson[v]=to;
         sz[v]+=sz[to];
@@ -77,7 +84,7 @@ void dfs2(int v,int p,int num)
     if(wson[v]) dfs2(wson[v],v,num);
     for(int i=0;i<G[v].size();i++)
     {
-        int to=G[v][i].to;
+        int to=G[v][i];
         if(to==p||to==wson[v]) continue;
         dfs2(to,v,to);
     }
@@ -86,21 +93,16 @@ void init()
 {
     tot=0;dfs1(1,1,1);
     dfs2(1,0,1);
-    memset(a,0,sizeof(a));
-    for(int i=0;i<n-1;i++)
-    {
-        u[i]=dep[u[i]]>dep[v[i]]?u[i]:v[i];
-        a[u[i]]=cost[i];
-    }
     tree.build(1,1,n);
 }
-void update(int k,int x)
+void update(int u,int v)
 {
-    tree.update(spos[k],x);
+    if(pa[u]!=v) swap(u,v);
+    tree.update(spos[u],-1);
 }
 int query_max(int u,int v)
 {
-    int res=0;
+    int res=-INF;
     while(top[u]!=top[v])
     {
         if(dep[top[u]]<dep[top[v]]) swap(u,v);
@@ -108,35 +110,51 @@ int query_max(int u,int v)
         u=pa[top[u]];
     }
     if(dep[u]<dep[v]) swap(u,v);
-    if(u!=v) res=max(res,tree.query_max(spos[wson[v]],spos[u]));
+    res=max(res,tree.query_max(spos[v],spos[u]));
+    return res;
+}
+int query_sum(int u,int v)
+{
+    int res=0;
+    while(top[u]!=top[v])
+    {
+        if(dep[top[u]]<dep[top[v]]) swap(u,v);
+        res+=tree.query_sum(spos[top[u]],spos[u]);
+        u=pa[top[u]];
+    }
+    if(dep[u]<dep[v]) swap(u,v);
+    res+=tree.query_sum(spos[v],spos[u]);
     return res;
 }
 char str[10];
 int x,y;
 int main()
 {
-    scanf("%d",&t);
-    while(t--)
+    scanf("%d",&n);
+    for(int i=0;i<n-1;i++)
     {
-        scanf("%d",&n);
-        for(int i=1;i<=n;i++)
-            G[i].clear();
-        for(int i=0;i<n-1;i++)
+        int u,v;
+        scanf("%d%d",&u,&v);
+        G[u].push_back(v);G[v].push_back(u);
+    }
+    init();
+    scanf("%d",&q);
+    q+=n-1;
+    while(q--)
+    {
+        scanf("%s",str);
+        if(str[0]=='W') 
         {
-            scanf("%d%d%d",&u[i],&v[i],&cost[i]);
-            G[u[i]].push_back((edge){v[i],cost[i]});G[v[i]].push_back((edge){u[i],cost[i]});
+            scanf("%d",&x);
+            printf("%d\n",query_sum(1,x));
         }
-        init();
-        while(scanf("%s",&str)!=EOF)
+        else
         {
-            if(str[0]=='D') break;
             scanf("%d%d",&x,&y);
-            if(str[0]=='C') update(u[x-1],y);
-            else printf("%d\n",query_max(x,y));
+            update(x,y);
         }
     }
     return 0;
 }
-
 
 
---
+++ CSA/80A.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+    > File Name: 80A.cpp
+    > Author: Roundgod
+    > Mail: wcysai@foxmail.com 
+    > Created Time: 2018-05-23 23:00:31
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+int n,k,a[MAXN];
+int main()
+{
+    for(int i=0;i<16;i++) scanf("%d",&a[i]);
+    int cnt=4;
+    for(int i=4;i<16;i++) if(a[i]+100>=a[i-1]) cnt++; else break;
+    printf("%d\n",cnt);
+    return 0;
+}
+
