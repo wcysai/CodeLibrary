c4e1db9f862d36a88f6bb0afe8cbbd37d7e0e094
wcysai <wcysai@foxmail.com>
2018-05-31 15:48:46 +0800

    finished CodeM18

--- Contests/CodeM18/D.cpp
+++ Contests/CodeM18/D.cpp
@@ -16,70 +16,88 @@
 #define S second
 using namespace std;
 typedef long long ll;
-typedef pair<long double,int> P;
-int n,m,k,C;
-int score[MAXM][MAXN],weight[MAXM];
+typedef pair<ll,int> P;
+ll n,m,k,C;
+ll score[MAXM][MAXN],weight[MAXM],save[MAXM];
 bool mark[MAXN];
-long double now[MAXN];
+ll now[MAXN];
 P rating[MAXN];
-int cnt[MAXN];
+ll cnt[MAXN];
 int main()
 {
-    scanf("%d%d%d%d",&n,&m,&k,&C);
+    scanf("%lld%lld%lld%lld",&n,&m,&k,&C);
     memset(mark,false,sizeof(mark));
-    int sum=0;
-    for(int i=0;i<m;i++) 
-        scanf("%d",&weight[i]);
-    int pid=-1,rid=-1;
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
+    ll sum=0;
+    for(ll i=0;i<m;i++) 
+        scanf("%lld",&weight[i]);
+    ll pid=-1,rid=-1;
+    for(ll i=0;i<n;i++)
+        for(ll j=0;j<m;j++)
         {
-            scanf("%d",&score[j][i]);
+            scanf("%lld",&score[j][i]);
             if(score[j][i]==-1) 
             {
                 pid=i;
                 rid=j;
             }
         }
-    for(int i=0;i<m;i++)
+    ll denom=1;
+    for(ll i=0;i<m;i++)
     {
         if(i==rid) continue;
-        int res=0;
-        for(int j=0;j<n;j++)
+        ll res=0;
+        for(ll j=0;j<n;j++)
             res=max(res,score[i][j]);
+        save[i]=res;
         if(res==0) continue;
-        for(int j=0;j<n;j++)
-            now[j]+=(long double)score[i][j]/res*weight[i];
+        denom*=res;
     }
-    for(int i=0;i<=C;i++)
+    for(ll i=0;i<m;i++)
+    {
+        if(i==rid||save[i]==0) continue;
+        for(ll j=0;j<n;j++)
+            now[j]+=score[i][j]*denom/save[i]*weight[i];
+    }
+    for(ll i=0;i<=C;i++)
     {
         score[rid][pid]=i;
-        int res=0;
-        for(int j=0;j<n;j++)
+        ll res=0;
+        for(ll j=0;j<n;j++)
             res=max(res,score[rid][j]);
-        for(int j=0;j<n;j++)
+        if(res==0)
         {
-            rating[j].F=now[j]+(res==0?0:(long double)score[rid][j]/res*weight[rid]);
-            rating[j].S=j;
+            for(ll j=0;j<n;j++)
+            {
+                rating[j].F=now[j];
+                rating[j].S=j;
+            }
+        }
+        else
+        {
+            for(ll j=0;j<n;j++)
+            {
+                rating[j].F=now[j]*res+denom*score[rid][j]*weight[rid];
+                rating[j].S=j;
+            }
         }
         sort(rating,rating+n);
-        for(int j=n-1;j>n-1-k;j--)
+        for(ll j=n-1;j>n-1-k;j--)
             cnt[rating[j].S]++;
         if(n==k) continue;
         if(rating[n-k].F==rating[n-1-k].F)
         {
-            int lid=INF,rid=-INF;
-            for(int i=0;i<n;i++)
-                if(abs(rating[i].F-rating[n-k].F)<eps)
+            ll lid=INF,rid=-INF;
+            for(ll i=0;i<n;i++)
+                if(rating[i].F==rating[n-k].F)
                 {
                     lid=min(lid,i);
                     rid=max(rid,i);
                 }
-            for(int i=lid;i<=rid;i++)
+            for(ll i=lid;i<=rid;i++)
                 mark[rating[i].S]=true;
         }
     }
-    for(int i=0;i<n;i++)
+    for(ll i=0;i<n;i++)
     {
         if(mark[i]) printf("3");
         else if(cnt[i]==C+1) printf("1");
