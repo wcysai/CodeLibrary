5f897d31eaff2cb769321fab159bfc2db129bea3
wcysai <wcysai@foxmail.com>
2018-06-04 18:53:44 +0800

    Codeforces 486(Div.3)

--- Atcoder/Grand/025/B.cpp
+++ Atcoder/Grand/025/B.cpp
@@ -7,7 +7,7 @@
 
 #include<bits/stdc++.h>
 #pragma GCC optimize(3)
-#define MAXN 300005
+#define MAXN 350005
 #define INF 1000000000
 #define MOD 998244353
 #define F first
@@ -15,12 +15,12 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-int n,a,b;
+ll n,a,b;
 ll k;
-int fact[MAXN],invf[MAXN];
-int pow_mod(int a,int i)
+ll fact[MAXN],invf[MAXN];
+ll pow_mod(ll a,ll i)
 {
-    int s=1;
+    ll s=1;
     while(i)
     {
         if(i&1) s=1LL*s*a%MOD;
@@ -29,23 +29,29 @@ int pow_mod(int a,int i)
     }
     return s;
 }
+ll comb(ll n,ll k)
+{
+    return (1LL*fact[n]*invf[k]%MOD)*invf[n-k]%MOD;
+}
 int main()
 {
-    scanf("%d%d%d%lld",&n,&a,&b,&k);
+    scanf("%lld%lld%lld%lld",&n,&a,&b,&k);
     fact[0]=invf[0]=1;
-    for(int i=1;i<=n;i++)
+    for(ll i=1;i<=n;i++)
         fact[i]=1LL*fact[i-1]*i%MOD;
     invf[n]=pow_mod(fact[n],MOD-2);
-    for(int i=n-1;i>=1;i--)
+    for(ll i=n-1;i>=1;i--)
         invf[i]=1LL*invf[i+1]*(i+1)%MOD;
-    int ans=0;
-    for(int i=0;i<=n;i++)
+    ll ans=0;
+    for(ll i=0;i<=n;i++)
     {
+        if(i*a>k) continue;
         if((k-1LL*i*a)%b!=0) continue;
         ll numa=i,numb=(k-1LL*i*a)/b;
-        if(min(numa,numb)>n) continue;
-        
+        if(max(numa,numb)>n) continue;
+        ans=(ans+1LL*comb(n,numa)*comb(n,numb))%MOD;
     }
+    printf("%lld\n",ans);
     return 0;
 }
 
