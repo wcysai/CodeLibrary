813446e5cf8d178787cd79069885df8f06e36ff8
wcysai <wcysai@foxmail.com>
2018-06-12 19:23:13 +0800

    Codeforces Edu 45

--- Codeforces/Edu/45/E.cpp
+++ Codeforces/Edu/45/E.cpp
@@ -8,38 +8,52 @@
 #pragma GCC optimize(3)
 #include<bits/stdc++.h>
 #define MAXN 2000005
-#define INF 1000000000
+#define INF 1000000000000000LL
 #define MOD 1000000007
 #define F first
 #define S second
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-int n,m,k;
-int a[MAXN],dp[MAXN];
+ll n,m,k;
+ll a[MAXN],pre[MAXN],nxt[MAXN];
 bool block[MAXN];
 int main()
 {
-    scanf("%d%d%d",&n,&m,&k);
+    scanf("%lld%lld%lld",&n,&m,&k);
     memset(block,false,sizeof(block));
-    for(int i=1;i<=m;i++)
+    for(ll i=1;i<=m;i++)
     {
-        int x;
-        scanf("%d",&x);
+        ll x;
+        scanf("%lld",&x);
         block[x]=true;
     }
-    for(int i=1;i<=k;i++)
-        scanf("%d",&a[i]);
-    for(int i=k-1;i>=1;i--)
-        a[i]=min(a[i],a[i+1]);
-    fill(dp,dp+n,INF);
-    dp[0]=0;
-    for(int i=0;i<n-1;i++)
+    for(ll i=1;i<=k;i++)
+        scanf("%lld",&a[i]);
+    if(block[0])
     {
-        if(block[i]) continue;
-        for(int j=1;j<=k;j++)
-            dp[i+j]=min(dp[i+j],dp[i]+a[j]);
+        puts("-1");
+        return 0;
     }
+    for(ll i=1;i<=2*n;i++)
+        pre[i]=block[i-1]?pre[i-1]:i-1;
+    ll maxi=1;
+    for(ll i=1;i<=n;i++)
+        maxi=max(maxi,i-pre[i]);
+    ll ans=INF;
+    for(ll i=maxi;i<=k;i++)
+    {
+        ll now=0,cnt=0;
+        bool f=true;
+        while(now<n)
+        {
+            now+=i;
+            cnt+=a[i];
+            if(block[now]) now=pre[now];
+        }
+        ans=min(ans,cnt);
+    }
+    if(ans==INF) puts("-1"); else printf("%lld\n",ans);
     return 0;
 }
 
