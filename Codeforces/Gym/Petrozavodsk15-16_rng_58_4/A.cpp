91ace26c4ebf262f754f38a92451fe33c42d61ca
wcysai <wcysai@foxmail.com>
2018-09-03 11:08:34 +0800

    Petrozavodsk15-16_rng_58_4

---
+++ Codeforces/Gym/Petrozavodsk15-16_rng_58_4/A.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Roundgod
+    > Mail: wcysai@foxmail.com 
+    > Created Time: 2018-09-02 14:01:23
+ ************************************************************************/
+
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+int n,k,a[MAXN];
+int main()
+{
+    return 0;
+}
+
