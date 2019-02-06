b9c88008fb1960328d38d7e0f7371fe18018207b
wcysai <wcysai@foxmail.com>
2019-02-06 17:56:55 +0800

    solved Codeforces Educational Round 59 F

--- Codeforces/Edu/59/F.cpp
+++ Codeforces/Edu/59/F.cpp
@@ -9,82 +9,52 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-struct edge{int to,cap,cost,rev;};
-int dist[MAXV],h[MAXV],prevv[MAXV],preve[MAXV];
-int n,V;
-int a[MAXN],b[MAXN],k[MAXN];
-vector<edge> G[MAXV];
-void add_edge(int from,int to,int cap,int cost)
+ll n;
+ll a[MAXN],b[MAXN],k[MAXN];
+ll w[MAXN][MAXN];
+//minimum weight bipartite matching
+ll km(ll n,ll m)
 {
-    G[from].push_back((edge){to,cap,cost,(int)G[to].size()});
-    G[to].push_back((edge){from,0,-cost,(int)G[from].size()-1});
-}
-int min_cost_flow(int s,int t)
-{
-    int res=0;
-    fill(h,h+V,0);
-    int f=0,ans=0;
-    for(int t=1;t<=n;t++)
+    vector<int> u(n+1),v(m+1),p(m+1),way(m+1);
+    for(ll i=1;i<=n;i++) 
     {
-        f++;
-        priority_queue<P,vector<P>,greater<P> >que;
-        fill(dist,dist+V,INF);
-        dist[s]=0;
-        que.push(P(0,s));
-        while(!que.empty())
+        p[0]=i;
+        ll j0=0;
+        vector<int> minv(m+1,INF);
+        vector<char> used(m+1,false);
+        do 
         {
-            P p=que.top(); que.pop();
-            int v=p.second;
-            if(dist[v]<p.first) continue;
-            for(int i=0;i<(int)G[v].size();i++)
-            {
-                edge &e=G[v][i];
-                if(e.cap>0&&dist[e.to]>dist[v]+e.cost+h[v]-h[e.to])
+            used[j0]=true;
+            ll i0=p[j0],delta=INF,j1=0;
+            for(ll j=1;j<=m;++j)
+                if(!used[j]) 
                 {
-                    dist[e.to]=dist[v]+e.cost+h[v]-h[e.to];
-                    prevv[e.to]=v;
-                    preve[e.to]=i;
-                    que.push(P(dist[e.to],e.to));
+                    ll cur=w[i0][j]-u[i0]-v[j];
+                    if(cur<minv[j]) minv[j]=cur,way[j]=j0;
+                    if(minv[j]<delta) delta=minv[j],j1=j;
                 }
-            }
-        }
-        if(dist[t]==INF)
+            for(ll j=0;j<=m;++j) if(used[j]) u[p[j]]+=delta,v[j]-=delta; else minv[j]-=delta;
+            j0=j1;
+        }while(p[j0]!=0);
+        do 
         {
-            return -1;
-        }
-        for(int v=0;v<V;v++) h[v]+=dist[v];
-        int d=f;
-        for(int v=t;v!=s;v=prevv[v])
-        {
-            d=min(d,G[prevv[v]][preve[v]].cap);
-        }
-        f-=d;
-        res+=d*h[t];
-        for(int v=t;v!=s;v=prevv[v])
-        {
-            edge &e=G[prevv[v]][preve[v]];
-            e.cap-=d;
-            G[v][e.rev].cap+=d;
-        }
-        if(f) break;
-        ans=max(ans,res);
+            ll j1=way[j0];
+            p[j0]=p[j1];
+            j0=j1;
+        }while(j0);
     }
-    return ans;
+    ll res=0;
+    for(ll i=1;i<=m;i++) res+=w[p[i]][i];
+    return -res;
 }
 int main()
 {
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++) scanf("%d%d%d",&a[i],&b[i],&k[i]);
-    V=2*n+2;
-    for(int i=1;i<=n;i++) add_edge(0,i,1,0);
-    for(int i=n+1;i<=2*n;i++) add_edge(i,2*n+1,1,0);
-    for(int i=1;i<=n;i++)
-        for(int j=n+1;j<=2*n;j++)
-        {
-            int days=min(j-n-1,k[i]);
-            add_edge(i,j,1,a[i]-days*b[i]);
-        }
-    printf("%d\n",min_cost_flow(0,2*n+1));
+    scanf("%lld",&n);
+    for(ll i=0;i<n;i++) scanf("%lld%lld%lld",&a[i],&b[i],&k[i]);
+    for(ll i=0;i<n;i++)
+        for(ll j=0;j<n;j++)
+            w[i+1][j+1]=-(max(0LL,a[j]-min(i,k[j])*b[j]));
+    printf("%lld\n",km(n,n));
     return 0;
 }
 
