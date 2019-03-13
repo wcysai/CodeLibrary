8a154f3811ef40b9c8d67e65ac6d2b9c48eec137
wcysai <wcysai@foxmail.com>
2019-03-13 10:22:41 +0800

    solved AGC 017 D Game on Tree

---
+++ Atcoder/Grand/017/D.cpp
@@ -0,0 +1,42 @@
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/priority_queue.hpp>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> P;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef __gnu_pbds::priority_queue<int,greater<int>,pairing_heap_tag> pq;
+int n;
+vector<int> G[MAXN];
+int sg[MAXN];
+void dfs(int v,int p)
+{
+    for(auto to:G[v])
+    {
+        if(to==p) continue;
+        dfs(to,v);
+        sg[v]^=(sg[to]+1);
+    }
+}
+int main()
+{
+    scanf("%d",&n);
+    for(int i=0;i<n-1;i++)
+    {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        G[u].push_back(v);G[v].push_back(u);
+    }
+    dfs(1,0);
+    if(sg[1]) puts("Alice"); else puts("Bob");
+    return 0;
+}
+
