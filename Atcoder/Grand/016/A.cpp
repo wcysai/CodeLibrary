a183f42e6c8e759afff0894c9e5657281227216d
wcysai <wcysai@foxmail.com>
2019-03-14 08:39:16 +0800

    solved AGC 016 A Shrinking

---
+++ Atcoder/Grand/016/A.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/priority_queue.hpp>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> P;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef __gnu_pbds::priority_queue<int,greater<int>,pairing_heap_tag> pq;
+int n;
+string str;
+int main()
+{
+    cin>>str;
+    n=str.size();
+    int ans=INF;
+    for(int i=0;i<26;i++)
+    {
+        int last=-1,cur=0;
+        for(int j=0;j<n;j++)
+        {
+            if(str[j]=='a'+i)
+            {
+                cur=max(cur,j-last-1);
+                last=j;
+            }
+        }
+        cur=max(cur,n-last-1);
+        ans=min(ans,cur);
+    }
+    printf("%d\n",ans);
+    return 0;
+}
+
