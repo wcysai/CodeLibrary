05070f34454871dfe7fab17be149219c6198c28d
wcysai <wcysai@foxmail.com>
2019-03-16 23:38:51 +0800

    AGC 031 A

---
+++ Atcoder/Grand/031/A.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/priority_queue.hpp>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int,int> P;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef __gnu_pbds::priority_queue<int,greater<int>,pairing_heap_tag> pq;
+int n,cnt[26];
+string str;
+int ans;
+int main()
+{
+    scanf("%d",&n);
+    cin>>str;
+    for(int i=0;i<n;i++) cnt[str[i]-'a']++;
+    ans=1;
+    for(int i=0;i<26;i++) ans=1LL*ans*(cnt[i]+1)%MOD;
+    ans--;if(ans<0) ans+=MOD;
+    printf("%d\n",ans);
+    return 0;
+}
+
