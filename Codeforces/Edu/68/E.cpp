3f25d7487e6064a54957c96ccef639870eba877c
wcysai <wcysai@foxmail.com>
2019-07-15 01:46:35 +0800

    upsolved E

--- Codeforces/Edu/68/E.cpp
+++ Codeforces/Edu/68/E.cpp
@@ -9,12 +9,22 @@
 #define F first
 #define S second
 using namespace std;
-using namespace __gnu_pbds;
 typedef long long ll;
 typedef pair<int,int> P;
 int n;
-vector<P> row[MAXN],col[MAXN];
-vector<int> ed[MAXN];
+struct node
+{
+    int x,l,r;
+};
+bool cmp(node p,node q)
+{
+    return p.x<q.x;
+}
+bool cmp2(node p,node q)
+{
+    return p.r<q.r;
+}
+vector<node> row,col;
 int bit[MAXN+1];
 int sum(int i)
 {
@@ -34,6 +44,7 @@ void add(int i,int x)
         i+=i&-i;
     }
 }
+vector<P> ed;
 int main()
 {
     scanf("%d",&n);
@@ -41,38 +52,35 @@ int main()
     {
         int a,b,c,d;
         scanf("%d%d%d%d",&a,&b,&c,&d);
-        a+=5000; b+=5000; c+=5000; d+=5000;
-        if(a==c) col[a].push_back(P(min(b,d),max(b,d)));
-        else row[b].push_back(P(min(a,c),max(a,c)));
+        a+=5001; b+=5001; c+=5001; d+=5001;
+        if(a==c) col.push_back((node){a,min(b,d),max(b,d)});
+        else row.push_back((node){b,min(a,c),max(a,c)});
     }
+    sort(col.begin(),col.end(),cmp2);
+    sort(row.begin(),row.end(),cmp);
     ll ans=0;
-    for(int i=0;i<=10000;i++)
+    for(int i=0;i<(int)row.size();i++)
     {
-        for(auto p:row[i])
+        int l=row[i].l,r=row[i].r,h=row[i].x;
+        ed.clear();
+        for(int j=0;j<(int)col.size();j++) 
         {
-            for(int j=p.F;j<=p.S;j++) 
-            {
-                for(auto q:col[j])
-                {
-                    if(q.F>i||q.S<i) continue;
-                    ed[q.S+1].push_back(j);
-                    add(j,1);
-                }
-            }
-            for(int j=i+1;j<=10001;j++)
-            {
-                for(auto x:ed[j]) add(x,-1);
-                for(auto q:row[j]) 
-                {
-                    int l=max(q.F,p.F);
-                    int r=min(q.S,p.S);
-                    if(l>r) continue;
-                    int x=sum(r)-sum(l-1);
-                    ans+=x*(x-1)/2;
-                }
-                ed[j].clear();
-            }
+            if(col[j].l>h||col[j].r<h) continue;
+            ed.push_back(P(col[j].r+1,col[j].x));
+            add(col[j].x,1);
         }
+        int now=0;
+        for(int j=i+1;j<(int)row.size();j++)
+        {
+            if(row[j].x==h) continue;
+            while(now<(int)ed.size()&&ed[now].F<=row[j].x) add(ed[now].S,-1),now++;
+            int lb=max(l,row[j].l);
+            int rb=min(r,row[j].r);
+            if(lb>rb) continue;
+            int x=sum(rb)-sum(lb-1);
+            ans+=x*(x-1)/2;
+        }
+        while(now<(int)ed.size()) add(ed[now].S,-1),now++;
     }
     printf("%lld\n",ans);
     return 0;
