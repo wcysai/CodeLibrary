79e9b605ae46da0cb2450d689ca5781afd57f3c7
wcysai <wcysai@foxmail.com>
2019-08-11 13:39:10 +0800

    Create gen.cpp

---
+++ Codeforces/Regular/576/gen.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define MAXN 400005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+int main()
+{
+    printf("%d\n",100000);
+    for(int i=1;i<=100000;i++) printf("%d %d\n",1,0);
+}
