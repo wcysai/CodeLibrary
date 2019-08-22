f5960c3a3851b273323ac9bf2111dc77b5541909
wcysai <wcysai@foxmail.com>
2019-08-22 22:16:52 +0800

    Update F.cpp

--- Contests/Petrozavodsk Winter-2014. Warsaw U Contest/F.cpp
+++ Contests/Petrozavodsk Winter-2014. Warsaw U Contest/F.cpp
@@ -7,63 +7,76 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-int n,q,l,w;
+ll n,q,l,w;
+ll offset=1000001;
 struct update
 {
-    int y,mult;
+    ll y,mult;
 };
 struct query
 {
-    int y,id,mult;
+    ll y,id,mult;
 };
-int bit[MAXN],n;
-int sum(int i)
+ll bit[2][MAXN];
+ll sum(ll id,ll i)
 {
-    int s=0;
+    ll s=0;
     while(i>0)
     {
-        s+=bit[i];
+        s+=bit[id][i];
         i-=i&-i;
     }
     return s;
 }
-void add(int i,int x)
+void add(ll id,ll i,ll x)
 {
-    while(i<=1000000)
+    while(i<MAXN)
     {
-        bit[i]+=x;
+        bit[id][i]+=x;
         i+=i&-i;
     }
 }
 vector<update> t[MAXN];
 vector<query> tq[MAXN];
+ll ans[MAXN];
 int main()
 {
-    scanf("%d%d%d%d",&n,&q,&l,&w);
-    for(int i=0;i<n;i++)
+    scanf("%lld%lld%lld%lld",&n,&q,&l,&w);
+    for(ll i=0;i<n;i++)
     {
-        int a,b,c,d;
-        scanf("%d%d%d%d",&a,&b,&c,&d);
-        t[a].push_back((update){d,1});
-        t[a].push_back((update){b,-1});
-        t[c].push_back((update){d,-1});
-        t[c].push_back((update){b,1});
+        ll a,b,c,d;
+        scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+        t[a+1].push_back((update){d,1});
+        t[a+1].push_back((update){b,-1});
+        t[c+1].push_back((update){d,-1});
+        t[c+1].push_back((update){b,1});
     }
-    for(int i=0;i<q;i++)
+    for(ll i=0;i<q;i++)
     {
-        int a,b,c,d;
-        scanf("%d%d",&a,&b);
+        ll a,b,c,d;
+        scanf("%lld%lld",&a,&b);
         c=a+l; d=b+w;
         tq[c].push_back((query){d,i,1});
         tq[c].push_back((query){b,i,-1});
         tq[a].push_back((query){d,i,-1});
         tq[a].push_back((query){b,i,1});
     }
-    ll sum=0;
-    for(int i=1;i<=1000000;i++)
+    for(ll i=1;i<=1000000;i++)
     {
-        for(auto upd:t[i]) add(t[i].F,t[i].S);
-        sum+=
+        for(auto upd:t[i])
+        {
+            add(0,offset-upd.y,-upd.mult*(i-1));
+            add(1,offset-upd.y,upd.mult);
+            printf("add0 %lld %lld\n",upd.y,-upd.mult*(i-1));
+            printf("add1 %lld %lld\n",upd.y,upd.mult);
+        }
+        for(auto qu:tq[i])
+        {
+            ans[qu.id]+=qu.mult*qu.y*(sum(0,offset-qu.y)+sum(1,offset-qu.y)*i);
+            printf("query %lld %lld %lld %lld ",qu.mult,qu.y,sum(0,offset-qu.y),sum(1,offset-qu.y));
+            printf("%lld\n",ans[qu.id]);
+        }
     }
+    for(ll i=0;i<q;i++) printf("%lld\n",ans[i]);
     return 0;
 }
