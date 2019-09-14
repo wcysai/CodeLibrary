e679e0c30f0efa8770f309fd26de990aa129efbb
wcysai <wcysai@foxmail.com>
2019-09-15 00:29:34 +0800

    added CF 584

---
+++ Codeforces/Regular/584/F.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define MAXN 200005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+int n,m;
+struct edge{int to,cost;};
+vector<edge> G[MAXN];
+int ans[MAXN];
+int d[MAXN];
+const
+void dijkstra(int s)
+{
+    priority_queue<P,vector<P>,greater<P> > que;
+    fill(d,d+V,INF);
+    d[s]=0;
+    que.push(P(0,s));
+    while(!que.empty())
+    {
+        P p=que.top(); que.pop();
+        int v=p.second;
+        if(d[v]<p.first) continue;
+        for(int i=0;i<G[v].size();i++)
+        {
+            edge e=G[v][i];
+            if(d[e.to]>d[v]+e.cost)
+            {
+                d[e.to]=d[v]+e.cost;
+                que.push(P(d[e.to],e.to));
+            }
+        }
+    }
+}
+int main()
+{
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=m;i++)
+    {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        G[u].push_back((edge){v,i}); G[v].push_back((edge){u,i});
+    }
+
+}
