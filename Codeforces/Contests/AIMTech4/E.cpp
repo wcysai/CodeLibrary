826ac3c30bbfcc3cfbd8207b91a438946a11ca12
wcysai <wcysai@foxmail.com>
2019-10-12 22:12:44 +0800

    Create E.cpp

---
+++ Codeforces/Contests/AIMTech4/E.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#define MAXV 3005
+#define MAXE 50000
+#define INF 1000000
+using namespace std;
+struct edge{int to,cap,rev;};
+int n,m,s,t;
+vector<edge> G[MAXV];
+int level[MAXV];
+int iter[MAXV];
+void add_edge(int from,int to,int cap)
+{
+    G[from].push_back((edge){to,cap,(int)G[to].size()});
+    G[to].push_back((edge){from,0,(int)G[from].size()-1});
+}
+void bfs(int s)
+{
+    memset(level,-1,sizeof(level));
+    queue<int> que;
+    level[s]=0;
+    que.push(s);
+    while(!que.empty())
+    {
+        int v=que.front(); que.pop();
+        for(int i=0;i<(int)G[v].size();i++)
+        {
+            edge &e=G[v][i];
+            if(e.cap>0&&level[e.to]<0)
+            {
+                level[e.to]=level[v]+1;
+                que.push(e.to);
+            }
+        }
+    }
+}
+
+int dfs(int v,int t,int f)
+{
+    if(v==t) return f;
+    for(int &i=iter[v];i<(int)G[v].size();i++)
+    {
+        edge &e=G[v][i];
+        if(level[v]<level[e.to]&&e.cap>0)
+        {
+            int d=dfs(e.to,t,min(f,e.cap));
+            if(d>0)
+            {
+                e.cap-=d;
+                G[e.to][e.rev].cap+=d;
+                return d;
+            }
+        }
+    }
+    return 0;
+}
+int max_flow(int s,int t)
+{
+    int flow=0;
+    for(;;)
+    {
+        bfs(s);
+        if(level[t]<0) return flow;
+        memset(iter,0,sizeof(iter));
+        int f;
+        while((f=dfs(s,t,INF))>0)
+          flow+=f;
+    }
+}
+int main()
+{
+    scanf
+    return 0;
+}
