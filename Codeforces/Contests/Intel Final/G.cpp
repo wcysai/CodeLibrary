42678399a1e6d42727e16a45652f26053dc91be5
wcysai <wcysai@foxmail.com>
2019-10-24 22:24:49 +0800

    ...

--- Codeforces/Contests/Intel Final/G.cpp
+++ Codeforces/Contests/Intel Final/G.cpp
@@ -1,1 +1,10 @@
-
+#include<bits/stdc++.h>
+#define MAXN 1000005
+#define MOD 1000000007
+#define INF 1000000000
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+struct edge{int to,cost;};
+int n,m;
+vector<edge> G[MAXN];
