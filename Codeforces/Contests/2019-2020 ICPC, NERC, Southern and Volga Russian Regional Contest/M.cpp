a128a82ffbd81ebc9d4bb21f19849abbc3c5b150
wcysai <wcysai@foxmail.com>
2019-10-29 21:43:38 +0800

    Update M.cpp

--- Codeforces/Contests/2019-2020 ICPC, NERC, Southern and Volga Russian Regional Contest/M.cpp
+++ Codeforces/Contests/2019-2020 ICPC, NERC, Southern and Volga Russian Regional Contest/M.cpp
@@ -1,1 +1,16 @@
-
+#include<bits/stdc++.h>
+#define MAXN 5005
+#define INF 1000000000
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+int n,k;
+bool isprime(int x)
+{
+    for(int i=2;i*i<=x;i++) if(x%i==0) return false;
+    return true;
+}
+int main()
+{
+    scanf("%d",&n);
+}
