14e2610417be920e8c81f9645409363dfa455cc9
wcysai <wcysai@foxmail.com>
2020-02-12 22:23:52 +0800

    Update L.cpp

---
+++ Contests/2019-2020 ICPC Southwestern European Regional Programming Contest (SWERC 2019-20)/L.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define MAXN 2000005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
