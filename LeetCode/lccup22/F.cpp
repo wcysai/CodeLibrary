6a81104cf3cc876d068cfcd6298d6c6dab416960
wcysai <wcysai@foxmail.com>
2022-04-23 17:22:58 +0800

    .

---
+++ LeetCode/lccup22/F.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int tot,sum[100005],maxi[100005],l[100005],r[100005],val[100005];
+    void dfs(TreeNode* t)
+    {
+        if(t==null) return;
+        tot++;
+        val[]
+    }
+public:
+    int getMaxLayerSum(TreeNode* root) {
+    
+    }
+};
